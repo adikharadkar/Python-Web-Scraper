a70b2bb715b5e20ce15377da5196826fbf76132e
Aditya Kharadkar <adityakharadkar27@gmail.com>
2022-09-05 15:36:30 +0530

    Extracted the first header

--- scraper.py
+++ scraper.py
@@ -2,4 +2,24 @@ from bs4 import BeautifulSoup
 import requests
 
 content = requests.get("https://ubuntu.com/security/notices").text
-print(content)
+
+try:
+	soup = BeautifulSoup(content, 'lxml')
+except:
+	print("Data could not be extracted using BeautifulSoup() function")
+
+titleName = soup.title.text
+try:
+	section = soup.find('section', class_='p-strip--suru-topped')
+except:
+	print("Section tag not found!")
+
+try:
+	header = section.find_all('div', class_='row')[0].div.h1.text
+except IndexError:
+	print("Index out of range")
+	header = None
+except:
+	header = None
+
+print(header)
