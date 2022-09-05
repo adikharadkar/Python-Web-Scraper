4f3d90d06faf92ff045dd5af381244cd72edd268
Aditya Kharadkar <adityakharadkar27@gmail.com>
2022-09-05 21:01:46 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -13,6 +13,8 @@ except:
 # Extracting the title name of the web page
 titleName = soup.title.text
 
+##########################################################################################################################################
+
 # Extracting the first section of the webpage
 # The first section contains the header and the summary
 try:
@@ -52,14 +54,123 @@ if summary is not None:
 	except:
 		links = None
 
+###################################################################################################################################
+
+# LATEST NOTICES
+
+# Extracting the whole section of articles
+try:
+	section_latest_notices = soup.find('section', class_='p-strip')
+except:
+	print("Section tag of latest notices could not be extracted")
+
+# Extracting the header i.e. "Latest Notices"
+try:
+	header_lates_notices = section_latest_notices.div.div.h2.text
+except:
+	header_lates_notices = None
+
+# Extracting all the articles of latest notices
+try:
+	articles_latest_notices = section_latest_notices.div.div.find_all('article')
+except:
+	articles_latest_notices = None
+
+lst_latest_notices = []
+lst_latest_notice = []
+print("-"*200)
+
+# Extracting the name, link, date and summary of the latest notice
+try:
+	for article in articles_latest_notices:
+		try:
+			latest_notices_name = article.h3.text
+		except:
+			latest_notices_name = None
+		try:
+			latest_notices_link = article.h3.a['href']
+		except:
+			latest_notices_link = None
+		try:
+			latest_notices_date = article.find_all('p')[0].text
+		except:
+			latest_notices_date = None
+		try:
+			latest_notices_summary = article.find_all('p')[1].text
+		except:
+			latest_notices_summary = None
+		
+		lst_latest_notice = [latest_notices_name, latest_notices_link, latest_notices_date, latest_notices_summary]
+		print(lst_latest_notice)
+
+		lst_latest_notices.append(lst_latest_notice)
+except IndexError:
+	print("Index is out of range")
+except:
+	lst_latest_notices = None
+
+# Extracting the Ubuntu Versions and storing them in a dictionary
+dict_ubuntu_versions = {}
+try:
+	for article in articles_latest_notices:
+		list_items = article.ul.find_all('li')
+		try:
+			for list_item in list_items:
+				name = list_item.a.text
+				link = list_item.a['href']
+				dict_ubuntu_versions.update({name: link})
+		except IndexError:
+			print("Index is out of range")
+		except:
+			dict_ubuntu_versions = None
+except IndexError:
+	print('Index is out of range')
+except:
+	print('Error - Ubuntu Versions')
+
+# Extracting all the CVE IDs and storing them in a dictionary
+dict_cve_ids = {}
+try:
+	for article in articles_latest_notices:
+		try:
+			latest_notices_cves = article.find_all('p')[2].small.find_all('a')
+		except:
+			print("CVE IDs not found!")
+		try:
+			for cve in latest_notices_cves:
+				try:
+					cve_name = cve.text
+				except:
+					cve_name = None
+
+				try:
+					cve_link = cve['href']
+				except:
+					cve_link = None
+				dict_cve_ids.update({cve_name: cve_link})
+		except IndexError:
+			print("Index is out of range")
+		except:
+			dict_cve_ids = None
+except IndexError:
+	print("Index is out of range")
+except:
+	dict_cve_ids = None
+
 # Printing the header, summary and links on the console
+print("-"*200)
 print("Header = {}".format(header))
 print("Summary = {}".format(summary))
-
+print("-"*200)
 try:
 	for link in links:
-		print(link)
+		print("Link: {}".format(link))
 except IndexError:
 	print("List index out of range")
 except:
 	print("Links = {}".format(None))
+print("-"*200)
+# print(lst_latest_notice)
+print("UBUNTU VERSION - {}".format(dict_ubuntu_versions))
+print("-"*200)
+print("CVE IDs: {}".format(dict_cve_ids))
